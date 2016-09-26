8d591e702294879ace64c5266c83b16e053dd6b9
Andreas Hahr <andreas.hahr@gmail.com>
2016-09-26 23:21:24 +0200

    new files, hashmap for saved states, utility function

--- GameTree.java
+++ GameTree.java
@@ -2,48 +2,141 @@ import java.util.*;
 
 public class GameTree {
 
-  public GameState gstate;
-  public int reachableXwin, reachableOwin;
+  public GameState gameState;
   public GameTree parent;
-  public List<GameTree> children;
+  public List<GameTree> = children;
 
-  public GameTree(GameState gstate) {
-    this.gstate = gstate;
+  public GameTree(GameState gState) {
+    this.gameState = gState;
+    this.parent = null;
     this.children = new LinkedList<GameTree>();
-    this.reachableXwin = 0;
-    this.reachableOwin = 0;
+  }
 
-    Vector<GameState> nextStates = new Vector<GameState>();
-    gstate.findPossibleMoves(nextStates);
+  public GameTree(GameState gState, GameTree parent) {
+    this.gameState = gState;
+    this.parent = parent;
+    this.children = new LinkedList<GameTree>();
+  }
 
-    for(int g = 0; g < nextStates.size(); g++) {
-      addChild(nextStates.elementAt(g));
+  public addChild(GameState gState) {
+    children.add(new GameTree(gState, this));
+  }
+
+  public buildTree(HashMap<GameState, GameTree> states, int depth) {
+    if(depth == 0) return;
+
+    if(this.children.size() == 0) {
+      Vector<GameState> nextStates = new Vector<GameState>();
+      for(int i = 0; i < nextStates.size(); i++) {
+        if(!states.containsKey(nextStates.get(i))) {
+          states.put(nextStates.get(i).clone(), stateGoodness(nextStates.get(i))); // clone for vector garbage collection?
+        }
+        addChild(states.get(nextStates.get(i)));
+      }
+      nextStates = null; // for garbage collecting the new vector?
     }
 
-    if(gstate.isEOG()) {
-      GameTree upWards = this.parent;
-      if(gstate.isXWin()) {
-        this.reachableXwin++;
-        while(upWards != null) {
-          upWards.reachableXwin++;
-          upWards = upWards.parent;
-        }
-      }
-      else if(gstate.isOWin()) {
-        this.reachableOwin++;
-        while(upWards != null) {
-          upWards.reachableOwin++;
-          upWards = upWards.parent;
-        }
-      }
+    for(int child = 0; child < children.size(); child++) {
+      children.get(child).buildTree(states, depth-1);
     }
   }
 
-  public GameTree addChild(GameState child) {
-    GameTree childNode = new GameTree(child);
-    childNode.parent = this;
-    this.children.add(childNode);
-    return childNode;
+  public int stateGoodness(GameState gState) {
+    int goodness = 0;
+    int myMarks, opMarks;
+    if(gState.getNextPlayer() == Constants.CELL_O) {
+      myMarks = Constants.CELL_X;
+      opMarks = Constants.CELL_O;
+    } else {
+      myMarks = Constants.CELL_O;
+      opMarks = Constants.CELL_X;
+    }
+    // points for rows
+    for(int r = 0; r < gState.BOARD_SIZE; r++) {
+      int rowPoints = 0;
+      for(int c = 0; c < gState.BOARD_SIZE; c++) {
+        if(gState.at(r, c) == myMarks) {
+          if(rowPoints < 0) {
+            rowPoints = 0;
+            break;
+          } else {
+            rowPoints = (rowPoints+1) * 2;
+          }
+        } else if(gState.at(r, c) == opMarks) {
+          if(rowPoints > 0) {
+            rowPoints = 0;
+            break;
+          } else {
+            rowPoints = (rowPoints-1) * 2;
+          }
+        }
+      }
+      goodness += rowPoints;
+    }
+    // points for columns
+    for(int c = 0; c < gState.BOARD_SIZE; c++) {
+      int colPoints = 0;
+      for(int r = 0; r < gState.BOARD_SIZE; r++) {
+        if(gState.at(r, c) == myMarks) {
+          if(colPoints < 0) {
+            colPoints = 0;
+            break;
+          } else {
+            colPoints = (colPoints+1) * 2;
+          }
+        } else if(gState.at(r, c) == opMarks) {
+          if(colPoints > 0) {
+            colPoints = 0;
+            break;
+          } else {
+            colPoints = (colPoints-1) * 2;
+          }
+        }
+      }
+      goodness += colPoints;
+    }
+    // points for diagonals
+    int diag1pts = 0;
+    int diag2pts = 0;
+    boolean diag1 = true;
+    boolean diag2 = true;
+    for(int i = 0; i < gState.BOARD_SIZE; i++) {
+      // diagonal 1
+      if(gState.at(i, i) == myMarks && diag1) {
+        if(diag1pts < 0) {
+          diag1pts = 0;
+          diag1 = false;
+        } else {
+          diag1pts = (diag1pts+1) * 2;
+        }
+      } else if(gState.at(i, i) == opMarks && diag1) {
+        if(diag1pts > 0) {
+          diag1pts = 0;
+          diag1 = false;
+        } else {
+          diag1pts = (diag1pts-1) * 2;
+        }
+      }
+      // diagonal 2
+      if(gState.at(i, gState.BOARD_SIZE-1-i) == opMarks && diag2) {
+        if(diag2pts < 0) {
+          diag2pts = 0;
+          diag2 = false;
+        } else {
+          diag2pts = (diag2pts+1) * 2;
+        }
+      } else if(gState.at(i, gState.BOARD_SIZE-1-i) == opMarks && diag2) {
+        if(diag2pts > 0) {
+          diag2pts = 0;
+          diag2 = false;
+        } else {
+          diag2pts = (diag2pts-1) * 2;
+        }
+      }
+      if(!diag1 && !diag2) break;
+    }
+    goodness += diag1pts + diag2pts;
+    return goodness;
   }
 
 }
--- Player.java
+++ Player.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Player {
 
+  private HashMap<GameState, Integer> states;
   private GameTree gt;
   /**
   * Performs a move
@@ -21,33 +22,14 @@ public class Player {
       return new GameState(gameState, new Move());
     }
 
+    Random random = new Random();
+    return nextStates.elementAt(random.nextInt(nextStates.size()));
+
     /**
     * Here you should write your algorithms to get the best next move, i.e.
     * the best next state. This skeleton returns a random move instead.
     */
-    GameTree gt = new GameTree(gameState);
 
-    int best = 0;
-    int bestIndex = 0;
-    if(gameState.getNextPlayer() == 0) {
-      for(int g = 0; g < gt.children.size(); g++) {
-        if(best < gt.children.get(g).reachableXwin - gt.children.get(g).reachableOwin) {
-          best = gt.children.get(g).reachableXwin - gt.children.get(g).reachableOwin;
-          bestIndex = 0;
-        }
-      }
-    }
-    else {
-      for(int g = 0; g < gt.children.size(); g++) {
-        if(best < gt.children.get(g).reachableOwin - gt.children.get(g).reachableXwin) {
-          best = gt.children.get(g).reachableOwin - gt.children.get(g).reachableXwin;
-          bestIndex = 0;
-        }
-      }
-    }
-    return gt.children.get(bestIndex).gstate;
-    /*Random random = new Random();
-    return nextStates.elementAt(random.nextInt(nextStates.size()));*/
   }
 
 }
